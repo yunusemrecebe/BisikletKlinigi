02273fafd46736002742ee88aab3cb18880123cc
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2021-02-26 16:40:03 +0300

    Otomatik slider eklendi, navbar eklendi, hakkımızda kısmına başlandı

--- WebUI/Views/script.js
+++ WebUI/Views/script.js
@@ -1,1 +1,11 @@
-// JavaScript source code
+var myFullpage = new fullpage('#fullpage', {
+	autoscrolling: false,
+	navigation: true,
+	controlArrows: false,
+	anchors: ['homepage','about','sales','contact'],
+	sectionsColor: ['gray', '#e9c46a', '#f4a261', '#e76f51']
+});
+
+setInterval(function() {
+	fullpage_api.moveSlideRight();;
+}, 3000);
