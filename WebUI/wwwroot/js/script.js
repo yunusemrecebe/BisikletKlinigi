adebd07b1e98ab4e99cf1c75815fc7f4a9413c8e
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2021-02-28 22:44:15 +0300

    css düzeltmeleri

--- WebUI/wwwroot/js/script.js
+++ WebUI/wwwroot/js/script.js
@@ -1,18 +1,3 @@
-function initMap() {
-
-    const bisikletMarket = { lat: -25.344, lng: 131.036 };
-    
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: bisikletMarket,
-    });
-    
-    const marker = new google.maps.Marker({
-        position: bisikletMarket,
-        map: map,
-    });
-}
-
 var galleryThumbs = new Swiper('.gallery-thumbs', {
     spaceBetween: 10,
     slidesPerView: 4,
