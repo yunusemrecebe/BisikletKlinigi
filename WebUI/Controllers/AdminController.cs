035b5485835f59a0d0f67eba59c0c3eaa360d8b7
Yunus Emre Cebe <yunusemrecebe@gmail.com>
2021-03-02 21:55:31 +0300

    test2

--- WebUI/Controllers/AdminController.cs
+++ WebUI/Controllers/AdminController.cs
@@ -52,49 +52,6 @@ namespace WebUI.Controllers
         }
 
         [HttpGet]
-        public IActionResult Messages()
-        {
-            if (HttpContext.Session.GetString("isUserLogin") != "true" || HttpContext.Session.GetInt32("userRole") == 1)
-            {
-                TempData["userIsNotLogin"] = "Yönetim Paneline Erişebilmek İçin Lütfen Giriş Yapınız!";
-                return RedirectToAction("Login");
-            }
-
-            var result = _contactUsService.GetAll();
-            if (result.Success)
-            {
-                ViewBag.messageStatus = result.Success;
-                return View(result.Data);
-            }
-
-            ViewBag.messageStatus = result.Success;
-            ViewBag.messagesError = result.Message;
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult DeleteMessage(int id)
-        {
-            if (HttpContext.Session.GetString("isUserLogin") != "true" || HttpContext.Session.GetInt32("userRole") == 1)
-            {
-                TempData["userIsNotLogin"] = "Yönetim Paneline Erişebilmek İçin Lütfen Giriş Yapınız!";
-                return RedirectToAction("Login");
-            }
-
-            var message = _contactUsService.GetById(id);
-
-            var result = _contactUsService.Delete(message.Data);
-            if (result.Success)
-            {
-                TempData["messagesError"] = result.Message;
-                return RedirectToAction("Messages");
-            }
-
-            TempData["messagesError"] = result.Message;
-            return RedirectToAction("Messages");
-        }
-
-        [HttpGet]
         public IActionResult Login()
         {
             return View();
--- WebUI/Controllers/ContactUsController.cs
+++ WebUI/Controllers/ContactUsController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Business.Abstract;
+using Microsoft.AspNetCore.Http;
 using WebUI.Models;
 
 namespace WebUI.Controllers
@@ -34,6 +35,49 @@ namespace WebUI.Controllers
             }
             TempData["ContactSendMessageResult"] = result.Message;
             return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Messages()
+        {
+            if (HttpContext.Session.GetString("isUserLogin") != "true" || HttpContext.Session.GetInt32("userRole") == 1)
+            {
+                TempData["userIsNotLogin"] = "Yönetim Paneline Erişebilmek İçin Lütfen Giriş Yapınız!";
+                return RedirectToAction("Login","Admin");
+            }
+
+            var result = _contactUsService.GetAll();
+            if (result.Success)
+            {
+                ViewBag.messageStatus = result.Success;
+                return View(result.Data);
+            }
+
+            ViewBag.messageStatus = result.Success;
+            ViewBag.messagesError = result.Message;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DeleteMessage(int id)
+        {
+            if (HttpContext.Session.GetString("isUserLogin") != "true" || HttpContext.Session.GetInt32("userRole") == 1)
+            {
+                TempData["userIsNotLogin"] = "Yönetim Paneline Erişebilmek İçin Lütfen Giriş Yapınız!";
+                return RedirectToAction("Login","Admin");
+            }
+
+            var message = _contactUsService.GetById(id);
+
+            var result = _contactUsService.Delete(message.Data);
+            if (result.Success)
+            {
+                TempData["messagesError"] = result.Message;
+                return RedirectToAction("Messages");
+            }
+
+            TempData["messagesError"] = result.Message;
+            return RedirectToAction("Messages");
         }
     }
 }
